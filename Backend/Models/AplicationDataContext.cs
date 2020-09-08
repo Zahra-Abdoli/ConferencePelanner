b83c446c7e698b8d97090b1495db3b5248aef90d
zahra1993a <33681568+zahra1993a@users.noreply.github.com>
2020-09-08 11:26:29 +0200

    Add project files.

---
+++ Backend/Models/AplicationDataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Models
+{
+    public class AplicationDataContext:DbContext
+    {
+        public AplicationDataContext(DbContextOptions<AplicationDataContext> options)
+       : base(options)
+        {
+
+        }
+
+        public DbSet<Speaker> Speakers { get; set; }
+    }
+
+}
+
